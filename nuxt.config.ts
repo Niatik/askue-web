2e5c03d9a3d78fa670e9c3b5b7263bf530004449
Nikita <11559709+Niatik@users.noreply.github.com>
2026-03-02 12:53:55 +0500

    Add `runtimeConfig` with public `apiBase`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['nuxt-auth-utils']
+  modules: ['nuxt-auth-utils'],
+
+  runtimeConfig: {
+
+    // Public keys that are exposed to the client
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api',
+    },
+  },
 })
+
